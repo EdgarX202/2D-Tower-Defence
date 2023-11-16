7a75a6c9ea8b96d22c7da097a141eb91ebe46096
InvisibleX30 <edgarpark30@gmail.com>
2023-11-16 12:39:13 +0000

    Healthbar UI

---
+++ 2D Tower Defence/Assets/Scripts/Projectile.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Enemy target;
+    private Tower parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToTarget();
+    }
+
+    public void Initialize(Tower parent)
+    {
+        this.target = parent.Target;
+        this.parent = parent;
+    }
+
+    private void MoveToTarget()
+    {
+        if(target != null && target.IsActive)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * parent.ProjectileSpeed);
+        }
+        else if (!target.IsActive)
+        {
+            // Reset projectile - send it back to object pool
+            GameManager.Instance.Pool.ObjectReset(gameObject);
+        }
+    }
+}
--- 2D Tower Defence/Assets/Scripts/Tower.cs
+++ 2D Tower Defence/Assets/Scripts/Tower.cs
@@ -11,6 +11,24 @@ public class Tower : MonoBehaviour
 
     private Queue<Enemy> enemies = new Queue<Enemy>();
 
+    private bool canAttack = true;
+    private float attackTime;
+
+    [SerializeField] private float projectileSpeed;
+    [SerializeField] private float cooldown;
+    [SerializeField] private string projectileType;
+
+    // Properties
+    public float ProjectileSpeed
+    {
+        get { return projectileSpeed; }
+    }
+
+    public Enemy Target
+    {
+        get { return target; }
+    }
+
     private void Start()
     {
         // Reference to sprite renderer
@@ -21,7 +39,7 @@ public class Tower : MonoBehaviour
         Attack();
 
         // Check which target is in range or null
-        Debug.Log(target);
+        //Debug.Log(target);
     }
 
     public void Select()
@@ -32,18 +50,49 @@ public class Tower : MonoBehaviour
 
     private void Attack()
     {
+        if(!canAttack)
+        {
+            attackTime += Time.deltaTime;
+
+            // When attack time is larger than cooldown, a tower can shoot again, set the time back to 0
+            if(attackTime >= cooldown)
+            {
+                canAttack = true;
+                attackTime = 0;
+            }
+        }
+
         if (target == null && enemies.Count > 0)
         {
             // Remove the enemy that left towers range from queue
             target = enemies.Dequeue();
         }
+
+        // If the target is in range and active, shoot once
+        if(target != null && target.IsActive)
+        {
+            if(canAttack)
+            {
+                Shoot();
+                canAttack = false;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        Projectile projectile = GameManager.Instance.Pool.getObject(projectileType).GetComponent<Projectile>();
+
+        // Spawn in the middle of the tower
+        projectile.transform.position = transform.position;
+        projectile.Initialize(this);
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Enemy")
         {
-            // Add an enemy to the queue
+            // Add an enemy to the queue when in range
             enemies.Enqueue(collision.GetComponent<Enemy>());
         }
     }
